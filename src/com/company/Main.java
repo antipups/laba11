b5b6b1df06213fca5be43b07b322fce1f7aafee0
antipups <nikitakurkurin@protonmail.ch>
2019-11-09 18:34:10 +0300

    9-ый вариант, 4-ая лаба график Ферма

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,21 +14,21 @@ import javax.swing.JLabel;
 
 public class Main {
     public static void main(String[] args) {
-        final int width = 512;
-        final int height = 512;
+        final int width = 512;  // размеры окна, ширина
+        final int height = 512;  // размеры окна, высота
         BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
         Graphics g = img.getGraphics();
         g.setColor(Color.black);
         g.fillRect(0, 0, width, height);
         g.setColor(Color.white);
-        final double A = 1;
-        final double scale = 128;
-        final double zoom = 50;
-        final double step = 1 / scale;
-        Point last = null;
+        final double A = 1;     // коэфициент
+        final double scale = 128;   // шаг для рисовки(пискели)
+        final double zoom = 50; // зум, чем больше тем виднее
+        final double step = 1 / scale;  // шаг для рисовки(пискели)
+        Point last = null;      // с начала до конца
         final Point origin = new Point(width/2, height/2);
 
-        for (double t = 0; t <= 4*PI; t+= step) {
+        for (double t = 0; t <= 4*PI; t+= step) {   // рисунок верхней части
             final double r = zoom * polarFunction(t, A);
             final int x = (int)round(r * cos(t));
             final int y = (int)round(r * sin(t));
@@ -40,7 +40,7 @@ public class Main {
             last = next;
         }
 
-        for (double t = 0; t <= 4*PI; t+= step) {
+        for (double t = 0; t <= 4*PI; t+= step) {   // отрцательной стороны
             final double r = zoom * polarFunction(t, A);
             final int x = (int)round(r * cos(t));
             final int y = (int)round(r * sin(t));
@@ -51,7 +51,7 @@ public class Main {
             }
             last = next;
         }
-
+        // далее настройка окна
         JFrame frame = new JFrame("testit");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(new JLabel(new ImageIcon(img)));
@@ -60,7 +60,7 @@ public class Main {
         frame.setVisible(true);
     }
 
-    public static double polarFunction(double t, double A) {
+    public static double polarFunction(double t, double A) {    // функция графика Ферма
         return A * sqrt(t);
     }
 }
